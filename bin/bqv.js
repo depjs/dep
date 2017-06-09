0397eb291d496f6e8c9391f110c75039bccb7904
Daijiro Wachi <daijiro.wachi@gmail.com>
2017-06-09 17:07:45 +0200

    added install command

--- bin/bqv.js
+++ bin/bqv.js
@@ -6,4 +6,28 @@ if (Number(process.version.substr(1, 1)) < 8) {
 }
 
 const yargs = require('yargs')
+const requireDirectory = require('require-directory')
+const commands = requireDirectory(module, '../lib')
 
+yargs.usage('Usage is here')
+
+yargs.help('help')
+  .alias('help', 'h')
+
+yargs.version(function () { return require('../package').version })
+  .alias('version', 'v')
+  .describe('version', 'Show version information')
+
+Object.keys(commands).forEach(function (c) {
+  var cmd = commands[c]
+  yargs.command(cmd)
+  if (cmd.aliases) {
+    cmd.aliases.forEach(function (alias) {
+      yargs.command(alias, false, cmd)
+    })
+  }
+})
+
+const argv = yargs.argv
+
+if (!argv._handled) yargs.showHelp()
