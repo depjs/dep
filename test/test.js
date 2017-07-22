e4119be7c4511cd65d4841e90da0b9510b70c5a5
Daijiro Wachi <daijiro.wachi@gmail.com>
2017-07-22 18:20:11 +0200

    add before and after script in the test

--- test/test.js
+++ test/test.js
@@ -3,7 +3,21 @@ const path = require('path')
 const { exec } = require('child_process')
 const tree = require('strong-npm-ls')
 const test = require('ava')
+const rimraf = require('rimraf')
 const fixtures = fs.readdirSync(path.join(__dirname, 'fixtures'))
+
+test.cb.before(t => {
+  let count = fixtures.length
+  t.plan(count)
+  fixtures.forEach(fixture => {
+    const modules = path.join(__dirname, 'fixtures', fixture, 'node_modules')
+    rimraf(modules, _ => {
+      count -= 1
+      t.pass()
+      if (count === 0) t.end()
+    })
+  })
+})
 
 test.cb(t => {
   let items = 3
@@ -22,6 +36,19 @@ test.cb(t => {
         t.is(deps, fix)
         if (count === 0) t.end()
       })
+    })
+  })
+})
+
+test.cb.after(t => {
+  let count = fixtures.length
+  t.plan(count)
+  fixtures.forEach(fixture => {
+    const modules = path.join(__dirname, 'fixtures', fixture, 'node_modules')
+    rimraf(modules, _ => {
+      count -= 1
+      t.pass()
+      if (count === 0) t.end()
     })
   })
 })
