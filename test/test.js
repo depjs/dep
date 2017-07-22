4f3bff679d9da386da01736eef44eff7459e4898
Daijiro Wachi <daijiro.wachi@gmail.com>
2017-07-22 18:14:52 +0200

    fix lint errors

--- test/test.js
+++ test/test.js
@@ -6,20 +6,18 @@ const test = require('ava')
 const fixtures = fs.readdirSync(path.join(__dirname, 'fixtures'))
 
 test.cb(t => {
-  let items = 2
+  let items = 3
   let count = fixtures.length * items
-	t.plan(count)
+  t.plan(count)
   fixtures.forEach(fixture => {
     const bin = path.join(__dirname, '..', 'bin', 'dep.js')
     const pkg = path.join(__dirname, 'fixtures', fixture)
-    const fix = fs.readFileSync(
-      path.join(__dirname, `fixtures/${fixture}/tree`),
-      'utf8'
-    )
+    const fix = fs.readFileSync(path.join(pkg, 'tree'), 'utf8')
     exec(`node ${bin} install`, {cwd: pkg}, (err, stdout, stderr) => {
+      t.ifError(err)
       tree.read(pkg, (err, out) => {
         count -= items
-        const deps = tree.printable(out,  Number.MAX_VALUE)
+        const deps = tree.printable(out, Number.MAX_VALUE)
         t.ifError(err)
         t.is(deps, fix)
         if (count === 0) t.end()
