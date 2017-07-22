772d0d1c02d4b56801bc201c62e4b09575b24832
Daijiro Wachi <daijiro.wachi@gmail.com>
2017-07-22 17:51:49 +0200

    use strong-npm-ls to test the tree

--- test/test.js
+++ test/test.js
@@ -1,5 +1,22 @@
+const fs = require('fs')
+const path = require('path')
+const { exec } = require('child_process')
+const tree = require('strong-npm-ls')
 const test = require('ava')
+const fixtures = fs.readdirSync(path.join(__dirname, 'fixtures'))
 
-test('foo', t => {
-  t.pass()
+test.cb(t => {
+  let count = fixtures.length
+	t.plan(count)
+  fixtures.forEach(fixture => {
+    const bin = path.join(__dirname, '..', 'bin', 'dep.js')
+    const pkg = path.join(__dirname, 'fixtures', fixture)
+    exec(`node ${bin} install`, {cwd: pkg}, (err, stdout, stderr) => {
+      count -= 1
+      tree.read(pkg, (e, d) => {
+        t.ifError(e)
+        if (count === 0) t.end()
+      })
+    })
+  })
 })
