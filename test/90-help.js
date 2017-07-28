7e6a23855edf95c0346cf674a45449536e807252
Daijiro Wachi <daijiro.wachi@gmail.com>
2017-07-29 00:19:50 +0200

    fix(test): test unhandled help

--- test/90-help.js
+++ test/90-help.js
@@ -10,3 +10,12 @@ test((t) => {
     t.end()
   })
 })
+
+test((t) => {
+  const bin = path.join(__dirname, '..', 'bin', 'dep.js')
+  exec(`node ${bin}`, (err, stdout, stderr) => {
+    t.ifError(err, 'help ran without error')
+    t.ok(stdout, 'help displayed a message')
+    t.end()
+  })
+})
