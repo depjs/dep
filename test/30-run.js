6a10926f3354f81552625eb976a64ae8fbe4cac1
Daijiro Wachi <daijiro.wachi@gmail.com>
2017-07-30 01:39:34 +0200

    fix(test): increase test coverage of run

--- test/30-run.js
+++ test/30-run.js
@@ -5,6 +5,13 @@ const pkg = path.join(__dirname, 'deps', 'run')
 const bin = path.join(__dirname, '..', 'bin', 'dep.js')
 
 test((t) => {
+  exec(`node ${bin} run`, {cwd: pkg}, (err, stdout, stderr) => {
+    t.ifError(err, 'run without error')
+    t.end()
+  })
+})
+
+test((t) => {
   exec(`node ${bin} run test`, {cwd: pkg}, (err, stdout, stderr) => {
     t.ifError(err, 'run test without error')
     t.end()
