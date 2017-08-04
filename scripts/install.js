16ba31485e1d6976d82dc53de786a2e372967488
Daijiro Wachi <daijiro.wachi@gmail.com>
2017-08-04 22:51:44 +0200

    fix(script): fix uninstall/install scripts

--- scripts/install.js
+++ scripts/install.js
@@ -1,28 +1,21 @@
-#!/usr/bin/env node
-
-const execFile = require('child_process').execFile
+const exec = require('child_process').exec
 const path = require('path')
 const fs = require('fs')
-const which = require('which')
 const execPath = process.execPath
 const binPath = path.dirname(execPath)
 const nodeModules = path.join(execPath, '../../lib/node_modules/dep')
-const pkgJSON = require('../package.json')
-const repository = pkgJSON.repository.url.replace('git+', '')
-const bin = path.join(nodeModules, pkgJSON.bin.dep)
+const repository = 'https://github.com/watilde/dep.git'
+const bin = path.join(nodeModules, 'bin/dep.js')
 
-which('git', (e, git) => {
-  if (e) throw new Error(e)
-  process.stdout.write(
-    'exec: ' + git + [' clone', repository, nodeModules].join(' ') + '\n'
-  )
-  execFile(git, ['clone', repository, nodeModules], (e) => {
-    if (e) throw new Error(e)
-    process.stdout.write('link: ' + bin + '\n')
-    process.stdout.write(' => ' + path.join(binPath, 'dep') + '\n')
-    fs.link(bin, path.join(binPath, 'dep'), (e) => {
-      if (e) throw new Error(e)
-      process.stdout.write('dep was installed successfully\n')
-    })
+process.stdout.write(
+  'exec: git' + [' clone', repository, nodeModules].join(' ') + '\n'
+)
+exec('git clone ' + repository + ' ' + nodeModules, (e) => {
+  if (e) throw e
+  process.stdout.write('link: ' + bin + '\n')
+  process.stdout.write(' => ' + path.join(binPath, 'dep') + '\n')
+  fs.link(bin, path.join(binPath, 'dep'), (e) => {
+    if (e) throw e
+    process.stdout.write('dep was installed successfully\n')
   })
 })
--- scripts/uninstall.js
+++ scripts/uninstall.js
@@ -1,20 +1,37 @@
-#!/usr/bin/env node
-
+const fs = require('fs')
 const path = require('path')
-const rimraf = require('rimraf')
 const execPath = process.execPath
 const binPath = path.dirname(execPath)
 const pkg = path.join(execPath, '../../lib/node_modules/dep')
 const bin = path.join(binPath, 'dep')
 
+const rmdir = (p) => {
+  if (fs.existsSync(p)) {
+    fs.readdirSync(p).forEach((file, index) => {
+      const current = path.join(p, file)
+      if (fs.lstatSync(current).isDirectory()) {
+        rmdir(current)
+      } else {
+        fs.unlinkSync(current)
+      }
+    })
+    fs.rmdirSync(p)
+  }
+}
+
 process.stdout.write(
   'remove: ' + pkg + '\n'
 )
-rimraf(pkg, (e) => {
-  process.stdout.write(
-    'remove: ' + bin + '\n'
-  )
-  rimraf(bin, (e) => {
-    process.stdout.write('dep was uninstalled successfully\n')
-  })
+try {
+  rmdir(pkg)
+} catch (e) {
+  throw e
+}
+
+process.stdout.write(
+  'remove: ' + bin + '\n'
+)
+fs.unlink(bin, (e) => {
+  if (e) throw e
+  process.stdout.write('dep was uninstalled successfully\n')
 })
