dc16b7427d8df2816de8340f497a4f50611bec0e
Daijiro Wachi <daijiro.wachi@gmail.com>
2017-08-05 03:45:52 +0200

    fix(scripts): add handling dat-node

--- scripts/install.js
+++ scripts/install.js
@@ -1,9 +1,11 @@
 const exec = require('child_process').exec
+const execFile = require('child_process').execFile
 const path = require('path')
 const fs = require('fs')
 const execPath = process.execPath
 const binPath = path.dirname(execPath)
 const nodeModules = path.join(execPath, '../../lib/node_modules/dep')
+const datNode = path.join(nodeModules, 'node_modules/dat-node')
 const repository = 'https://github.com/watilde/dep.git'
 const bin = path.join(nodeModules, 'bin/dep.js')
 
@@ -16,6 +18,10 @@ exec('git clone ' + repository + ' ' + nodeModules, (e) => {
   process.stdout.write(' => ' + path.join(binPath, 'dep') + '\n')
   fs.link(bin, path.join(binPath, 'dep'), (e) => {
     if (e) throw e
-    process.stdout.write('dep was installed successfully\n')
+    process.stdout.write('install: dat-node\n')
+    execFile(bin, ['install'], {cwd: datNode}, (e) => {
+      if (e) throw e
+      process.stdout.write('dep was installed successfully\n')
+    })
   })
 })
