cf13f4b2e5cdb03fc183c47c6cf26445fcb5f40f
Daijiro Wachi <daijiro.wachi@gmail.com>
2017-08-05 13:10:02 +0200

    fix(scripts): add native build processes to install script

--- scripts/install.js
+++ scripts/install.js
@@ -8,6 +8,7 @@ const nodeModules = path.join(execPath, '../../lib/node_modules/dep')
 const datNode = path.join(nodeModules, 'node_modules/dat-node')
 const repository = 'https://github.com/watilde/dep.git'
 const bin = path.join(nodeModules, 'bin/dep.js')
+const nodeGyp = reuqire(path.join(nodeModules, 'lib/utils/node-gyp'))
 
 process.stdout.write(
   'exec: git' + [' clone', repository, nodeModules].join(' ') + '\n'
@@ -17,11 +18,16 @@ exec('git clone ' + repository + ' ' + nodeModules, (e) => {
   process.stdout.write('link: ' + bin + '\n')
   process.stdout.write(' => ' + path.join(binPath, 'dep') + '\n')
   fs.link(bin, path.join(binPath, 'dep'), (e) => {
+    const sodium = path.join(datNode, 'node_modules/sodium-native')
+    const utp = path.join(datNode, 'node_modules/utp-native')
     if (e) throw e
     process.stdout.write('install: dat-node\n')
-    execFile(bin, ['install'], {cwd: datNode}, (e) => {
-      if (e) throw e
+    try {
+      nodeGyp({cwd: sodium})
+      nodeGyp({cwd: utp})
       process.stdout.write('dep was installed successfully\n')
-    })
+    } catch (e) {
+      throw e
+    }
   })
 })
