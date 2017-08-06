bfa004ebad5446ccdfd8c0d83a1a6593460018fb
Daijiro Wachi <daijiro.wachi@gmail.com>
2017-08-06 16:27:59 +0200

    fix(scirpts): update install build process

--- scripts/install.js
+++ scripts/install.js
@@ -18,20 +18,20 @@ exec('git clone ' + repository + ' ' + nodeModules, (e) => {
   fs.link(bin, path.join(binPath, 'dep'), (e) => {
     if (e) throw e
     const nodeGyp = require(path.join(nodeModules, 'lib/utils/node-gyp'))
+    const nodeGypBuild = require(path.join(datNode, '.bin/node-gyp-build'))
     const sodium = path.join(datNode, 'node_modules/sodium-native')
+    const sodiumBuild = nodeGypBuild + ' "node preinstall.js" "node postinstall.js"'
     process.stdout.write('build: ' + sodium + '\n')
-    try {
-      nodeGyp({cwd: sodium})
-    } catch (e) {
-      throw e
-    }
-    const utp = path.join(datNode, 'node_modules/utp-native')
-    process.stdout.write('build: ' + utp + '\n')
-    try {
-      nodeGyp({cwd: utp})
-      process.stdout.write('dep was installed successfully\n')
-    } catch (e) {
-      throw e
-    }
+    exec(sodiumBuild, {cwd: sodium}, (e) => {
+      if (e) throw e
+      const utp = path.join(datNode, 'node_modules/utp-native')
+      process.stdout.write('build: ' + utp + '\n')
+      try {
+        nodeGyp({cwd: utp})
+        process.stdout.write('dep was installed successfully\n')
+      } catch (e) {
+        throw e
+      }
+    })
   })
 })
