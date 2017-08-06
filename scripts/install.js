ef25188675fffafb6bc8641a59cb35ce8414162c
Daijiro Wachi <daijiro.wachi@gmail.com>
2017-08-06 16:49:16 +0200

    fix(dep): ignore node_modules in dat-node

--- scripts/install.js
+++ scripts/install.js
@@ -1,37 +1,25 @@
 const exec = require('child_process').exec
+const execFile = require('child_process').execFile
 const path = require('path')
 const fs = require('fs')
 const execPath = process.execPath
 const binPath = path.dirname(execPath)
-const nodeModules = path.join(execPath, '../../lib/node_modules/dep')
-const datModules = path.join(nodeModules, 'node_modules/dat-node/node_modules')
+const dep = path.join(execPath, '../../lib/node_modules/dep')
+const datNode = path.join(dep, 'node_modules/dat-node')
 const repository = 'https://github.com/watilde/dep.git'
-const bin = path.join(nodeModules, 'bin/dep.js')
+const bin = path.join(dep, 'bin/dep.js')
 
 process.stdout.write(
-  'exec: git' + [' clone', repository, nodeModules].join(' ') + '\n'
+  'exec: git' + [' clone', repository, dep].join(' ') + '\n'
 )
-exec('git clone ' + repository + ' ' + nodeModules, (e) => {
+exec('git clone ' + repository + ' ' + dep, (e) => {
   if (e) throw e
   process.stdout.write('link: ' + bin + '\n')
   process.stdout.write(' => ' + path.join(binPath, 'dep') + '\n')
   fs.symlink(bin, path.join(binPath, 'dep'), (e) => {
     if (e) throw e
-    const nodeGyp = require(path.join(nodeModules, 'lib/utils/node-gyp'))
-    const nodeGypBuild = path.join(datModules, '.bin/node-gyp-build')
-    const sodium = path.join(datModules, 'sodium-native')
-    const sodiumBuild = nodeGypBuild + ' "node preinstall.js" "node postinstall.js"'
-    process.stdout.write('build: ' + sodium + '\n')
-    exec(sodiumBuild, {cwd: sodium}, (e) => {
+    execFile(bin, ['install'], {cwd: datNode}, (e) => {
       if (e) throw e
-      const utp = path.join(datModules, 'utp-native')
-      process.stdout.write('build: ' + utp + '\n')
-      try {
-        nodeGyp({cwd: utp})
-        process.stdout.write('dep was installed successfully\n')
-      } catch (e) {
-        throw e
-      }
     })
   })
 })
