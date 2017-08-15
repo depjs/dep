5580fe682b116fcde958f1d2b2c601facc8f2f25
Daijiro Wachi <daijiro.wachi@gmail.com>
2017-08-15 20:39:02 +0200

    fix(test): add test for install --only

--- test/11-install-only.js
+++ test/11-install-only.js
@@ -4,23 +4,23 @@ const exec = require('child_process').exec
 const tree = require('strong-npm-ls')
 const test = require('tap').test
 const bin = path.join(__dirname, '..', 'bin', 'dep.js')
-const pkg = path.join(__dirname, 'deps/install-save')
+const pkg = path.join(__dirname, 'deps/install-only')
 const pkgJSON = require(path.join(pkg, 'package.json'))
 
 test((t) => {
-  exec(`node ${bin} install --save=prod text-table`, {cwd: pkg}, (err, stdout, stderr) => {
+  exec(`node ${bin} install --only=prod`, {cwd: pkg}, (err, stdout, stderr) => {
     t.ifError(err, `${pkgJSON.name}: install ran without error`)
     tree.read(pkg, (err, out) => {
       t.ifError(err, `${pkgJSON.name}: tree could be read`)
       const deps = out.dependencies
-      t.ok(deps['text-table'], `${pkgJSON.name}: deps are installed`)
+      t.ok(deps['happy-birthday'], `${pkgJSON.name}: deps are installed`)
       t.end()
     })
   })
 })
 
 test((t) => {
-  exec(`node ${bin} install --save=dev quack-array`, {cwd: pkg}, (err, stdout, stderr) => {
+  exec(`node ${bin} install --only=dev`, {cwd: pkg}, (err, stdout, stderr) => {
     t.ifError(err, `${pkgJSON.name}: install ran without error`)
     tree.read(pkg, (err, out) => {
       t.ifError(err, `${pkgJSON.name}: tree could be read`)
@@ -29,12 +29,4 @@ test((t) => {
       t.end()
     })
   })
-})
-
-test((t) => {
-  var data = pkgJSON
-  delete data.dependencies['text-table']
-  delete data.devDependencies
-  fs.writeFileSync(path.join(pkg, 'package.json'), JSON.stringify(data, 2, 2) + '\n')
-  t.end()
 })
