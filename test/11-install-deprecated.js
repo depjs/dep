f9ccb47cf95886a33df317005c578f9b7f5e5a8c
Daijiro Wachi <daijiro.wachi@gmail.com>
2017-09-17 22:58:18 +0200

    fix(test): use execFile

--- test/11-install-deprecated.js
+++ test/11-install-deprecated.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const exec = require('child_process').exec
+const execFile = require('child_process').execFile
 const tree = require('strong-npm-ls')
 const test = require('tap').test
 const bin = path.join(__dirname, '..', 'bin', 'dep.js')
@@ -7,7 +7,7 @@ const pkg = path.join(__dirname, 'deps/install-deprecated')
 const pkgJSON = require(path.join(pkg, 'package.json'))
 
 test((t) => {
-  exec(`node ${bin} install`, {cwd: pkg}, (err, stdout, stderr) => {
+  execFile(bin, ['install'], {cwd: pkg}, (err, stdout, stderr) => {
     t.ifError(err, `${pkgJSON.name}: install ran without error`)
     t.has(stdout, 'minimatch@0.0.1')
     tree.read(pkg, (err, out) => {
--- test/90-version.js
+++ test/90-version.js
@@ -1,10 +1,10 @@
 const path = require('path')
-const exec = require('child_process').exec
+const execFile = require('child_process').execFile
 const test = require('tap').test
 const bin = path.join(__dirname, '..', 'bin', 'dep.js')
 
 test((t) => {
-  exec(`node ${bin} -v`, (err, stdout, stderr) => {
+  execFile(bin, ['-v'], (err, stdout, stderr) => {
     t.ifError(err, 'version ran without error')
     t.ok(stdout, 'version displayed a message')
     t.end()
