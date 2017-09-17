49d57c4689bc296bc640e3259b28b2e04a918fac
Daijiro Wachi <daijiro.wachi@gmail.com>
2017-09-17 23:16:04 +0200

    fix(test): remove debug log

--- test/01-chmod.js
+++ test/01-chmod.js
@@ -1,16 +1,13 @@
 const fs = require('fs')
 const path = require('path')
 const test = require('tap').test
+const isRoot = require('../lib/utils/is-root')
 
 test((t) => {
-  fs.chmodSync(path.join(__dirname, '../.nyc_output'), '7777')
+  if (!isRoot()) return t.end()
+  fs.chmodSync(path.join(__dirname, '../.nyc_output'), '0755')
   const files = fs.readdirSync(path.join(__dirname, '../.nyc_output'))
   files.forEach((file) => {
-    fs.chmodSync(path.join(__dirname, `../.nyc_output/${file}`), '7777')
-  })
-  require('child_process').exec('ls -la .nyc_output', (e, out) => {
-    console.log('ls -la')
-    console.log(out)
-    t.end()
+    fs.chmodSync(path.join(__dirname, `../.nyc_output/${file}`), '0755')
   })
 })
