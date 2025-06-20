153988005deed256e4c7f6e913a831fc32ca7599
Daijiro Wachi <daijiro.wachi@gmail.com>
2025-06-20 23:35:18 +0900

    chore: downgrade fs-extra

--- node_modules/fs-extra/lib/remove/index.js
+++ node_modules/fs-extra/lib/remove/index.js
@@ -2,13 +2,18 @@
 
 const fs = require('graceful-fs')
 const u = require('universalify').fromCallback
+const rimraf = require('./rimraf')
 
 function remove (path, callback) {
-  fs.rm(path, { recursive: true, force: true }, callback)
+  // Node 14.14.0+
+  if (fs.rm) return fs.rm(path, { recursive: true, force: true }, callback)
+  rimraf(path, callback)
 }
 
 function removeSync (path) {
-  fs.rmSync(path, { recursive: true, force: true })
+  // Node 14.14.0+
+  if (fs.rmSync) return fs.rmSync(path, { recursive: true, force: true })
+  rimraf.sync(path)
 }
 
 module.exports = {
